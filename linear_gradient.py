5323b6b7b929ab21e6e537ba9a307ab453b6c064
dilirylis <dilirylis@gmail.com>
2022-10-08 22:04:46 +0800

    add comments

--- linear_gradient.py
+++ linear_gradient.py
@@ -11,13 +11,14 @@ assert image.shape[0] == image.shape[1]
 color1 = [255, 128, 0]
 color2 = [0, 128, 255]
 
-angle = 90 + 90 + 45
-sin = np.sin(np.deg2rad(angle))
+angle = 90 + 90 + 45  # чтобы градиент начинался с голубого цвета как в задании, поворачиваем на 135 градусов. можно ввести любой градус
+sin = np.sin(np.deg2rad(angle))  # переводим в радианы для последующих вычислений
 cos = np.cos(np.deg2rad(angle))
 
+# делаем два цикла, для того, чтобы идти по x и y. учитываем все четверти тригонометрического круга с помощью функций min(), max()
 for i, v in enumerate(np.linspace(min(0, sin), max(0, sin), image.shape[0])):
   for i2, v2 in enumerate(np.linspace(min(0, cos), max(0, cos), image.shape[1])):
-    t = (abs(v) + abs(v2)) / (abs(sin) + abs(cos))
+    t = (abs(v) + abs(v2)) / (abs(sin) + abs(cos))  # рассчитываем переход цвета и нормализуем  
     
     r = lerp(color1[0], color2[0], t)
     g = lerp(color1[1], color2[1], t)
